df02cfb2bd3f8408ea24501d2bdc3b4c482b97ba
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-09-10 19:49:18 -0400

    added react router

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import TeamList from "./Component/TeamList";
+import {Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   const [teamList, setTeamList] = useState()
+  
+  useEffect(() => {
+    setTeamList()
+  },[])
   return (
     <div className="App">
-     
+     <Route exact path='/' render={props => <TeamList {...props} teamList={teamList} /> } />
     </div>
   );
 }
---
+++ team-builder/src/Component/TeamList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+function TeamList({teamList}) {
+    
+    return (
+        <div>
+           {teamList.map(member => (
+               <div key={member.name}>
+                   <h1>Team Member</h1>
+                   <h2>{member.name}</h2>
+                   <h2>{member.email}</h2>
+                   <h2>{member.role}</h2>
+                   <h2>{member.location}</h2>
+               </div>
+           ))} 
+        </div>
+    )
+}
+
+export default TeamList
