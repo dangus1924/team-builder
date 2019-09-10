56bcdd540cfcd8825e54308056cf55fd2d5dd97c
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-09-10 19:57:57 -0400

    added data.js for the data names

---
+++ team-builder/src/Data.js
@@ -0,0 +1,21 @@
+export const data = [
+    {
+        name: 'Darren Angus',
+        email: 'dangus1924@gmail.com',
+        role: 'Full-stack student',
+        location: 'Neptune, NJ'
+    },
+
+    {
+        name: 'Dave williams',
+        email: 'dwilliams1924@gmail.com',
+        role: 'Full-stack student',
+        location: ' Neptune City, NJ'
+    },
+    {
+        name: 'Dale fray',
+        email: 'dfray1924@gmail.com',
+        role: 'Full-stack student',
+        location: 'Loven, NJ'
+    }
+]
